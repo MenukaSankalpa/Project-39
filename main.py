23e409c32b5ce42d2080c382b9226c375dea95c8
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-17 09:08:22 +0530

    def speak made

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import pyttsx3 
 import speech_recognition as sr 
 
+#import pyttsx3
+#print("pyttsx3 is installed and working!")
+
 def initialize_engine():
     engine = pyttsx3.init("sapi5")
     voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[1].id)
+    engine.setProperty('voice', voices[0].id)
     rate = engine.getProperty('rate')
     engine.setProperty('rate', rate-50)
     volume = engine.getProperty('volume')
     engine.setProperty('volume', volume+0.25)
-    return engine 
+    return engine
+
+def speak(text):
+    engine = initialize_engine()
+    engine.say(text)
+    engine.runAndWait()
+
+speak("Hello, I'm Menu")  
+
